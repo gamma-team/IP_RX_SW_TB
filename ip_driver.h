ef0f6404a188912cc39229f585fe62dd2b164b3b
Justin-Samstag <justin.samstag@gmail.com>
2017-03-27 19:35:12 -0400

    Update ip_driver.h

--- ip_driver.h
+++ ip_driver.h
@@ -2,14 +2,21 @@
 #include "stdarg.h"
 
 void set_cust_ip_reg(int baseaddr, int regnum, int value);
-int get_cust_ip_reg(int baseaddr, int regnum);
+unsigned int get_cust_ip_reg(int baseaddr, int regnum);
 void set_start_bit(int baseaddr);
 void clr_start_bit(int baseaddr);
-void set_end_bit(int baseaddr);
-void clr_end_bit(int baseaddr);
+void set_single_exec(int baseaddr);
+void set_multiple_exec(int baseaddr, int numExec);
+void set_payload_seq(int baseaddr);
+void set_payload_red(int baseaddr);
+unsigned int get_count_in(int baseaddr);
+unsigned int get_count_out(int baseaddr);
+unsigned int get_output_length(int baseaddr);
+unsigned int is_output_started( int baseaddr);
+unsigned int is_output_finished(int baseaddr);
+
 void set_header(int baseaddr, int hdr_1, int hdr_2, int hdr_3, int hdr_4, int hdr_5, int num_opt, ...);
-int get_header_length(int baseaddr);
+unsigned int get_header_length(int baseaddr);
 void set_packet_data(int baseaddr, int num_half_words, ...);
-int get_return_status(int baseaddr);
-int get_return_data_length(int baseaddr);
+void get_output_data(int baseaddr, int* rtn);
 
